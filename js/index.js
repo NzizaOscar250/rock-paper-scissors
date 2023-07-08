e07aa4afccf72a275d14e3ec388a718b69de81ff
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-07-09 01:38:36 +0200

    The playRound function takes the player's selection and the computer's selection as parameters

--- js/index.js
+++ js/index.js
@@ -5,3 +5,23 @@ function getComputerChoice() {
     return choices[randomIndex];
   }
   
+
+
+  
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+  
+    if (playerSelection === computerSelection) {
+      return "It's a tie!";
+    } else if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+      return "You win! " + playerSelection + " beats " + computerSelection;
+    } else {
+      return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+  }
+ 

ad59eb527c6320b1ef5289ebaf6fc5edb106a48a
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-07-09 01:39:55 +0200

    The game function handles the overall game logic. It initializes variables for the player's score and the computer's score.

--- js/index.js
+++ js/index.js
@@ -24,4 +24,34 @@ function playRound(playerSelection, computerSelection) {
       return "You lose! " + computerSelection + " beats " + playerSelection;
     }
   }
- 
+  
+  function game() {
+    var playerScore = 0;
+    var computerScore = 0;
+  
+    for (var round = 1; round <= 5; round++) {
+      var playerSelection = prompt("Enter your choice: rock, paper, or scissors");
+      var computerSelection = getComputerChoice();
+      var result = playRound(playerSelection, computerSelection);
+  
+      if (result.includes("win")) {
+        playerScore++;
+      } else if (result.includes("lose")) {
+        computerScore++;
+      }
+  
+      console.log("Round " + round + ": " + result);
+    }
+  
+    if (playerScore > computerScore) {
+      console.log("You win the game!");
+    } else if (playerScore < computerScore) {
+      console.log("You lose the game!");
+    } else {
+      console.log("It's a tie! The game ends in a draw.");
+    }
+  }
+  
+  // Start the game
+  game();
+  
